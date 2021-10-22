7ea9cc27d901cdea140f2e8e606664354547d9f9
Malik <malikbens@live.fr>
2021-10-22 16:28:43 +0200

    upload of the portfolio

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+import { FaGithub,  FaLinkedin } from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export default function Socialicons(params) {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        
+        <li>
+          <a href={socialprofils.github}>
+            <FaGithub />
+          </a>
+        </li>
+        
+        <li>
+          <a href={socialprofils.linkedin}>
+            <FaLinkedin />
+          </a>
+        </li>
+      </ul>
+      <p>Contactez-moi</p>
+    </div>
+  );
+}
---
+++ src/content_option.js
@@ -0,0 +1,138 @@
+const logoimg =  "./src/assets/logo.png" ;
+const meta = {
+    title: "",
+    description: "Bonjour je suis Malik Benserradj , Développeur Freelance basé dans le Var . Si vous vez besoin d'un développeur afin de réaliser vos projets , n'hésitez pas à me contacter",
+};
+
+const introdata = {
+    name:'Malik BENS ',
+    title: "Developpeur Fullstack Freelance",
+    animated: {
+        first: "JavaScript",
+        second: "React.js",
+        third: "Node.js",
+        fourth: "Express.js",
+        fifth: "MongoDB/Mysql",
+        sixth: "Wordpress/Prestashop"
+    },
+    description: "hbihkjnj",
+    img_url: "",
+};
+
+const dataabout = {
+    title: "Mon parcour : ",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Développeur Freelance ",
+        date: "2021",
+    },
+    {
+        jobtitle: "Formation AFPA Développeur Fullstack",
+        date: "2020",
+    },
+    {
+        jobtitle: "Photographe",
+        date: "2017",
+    },
+];
+
+const skills = [{
+        name: "HTML/CSS",
+        value: 80,
+    },
+    {
+        name: "JavaScript",
+        value: 85,
+    },
+    {
+        name: "React.js",
+        value: 80,
+    },
+    {
+        name: "Node.js",
+        value: 60,
+    },
+    {
+        name: "Express",
+        value: 60,
+    },
+    {
+        name: " MongoDB",
+        value: 70,
+    },
+    {
+        name: "MySql",
+        value: 80,
+    },
+    {
+        name: "Git",
+        value:80,
+    },
+    {
+        name:"Wordpress",
+        value:95,
+    },
+    {
+        name:"Prestashop",
+        value:75,
+    }
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress",
+        description: "Création de site Wordpress clef en main",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/Gn4Tj9Y/cityprod360.png",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/nmT2NFj/ethics-formation.png",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/BPbgN3p/gontran-dessagnes.png",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: " malik.benserradj@gmail.com",
+    YOUR_FONE: "06.62.14.63.12",
+    description: "Disponible la semaine et le week-end , n'hésitez pas à me contacter pour tout renseignement !",
+    YOUR_SERVICE_ID: "service_pafvq2p",
+    YOUR_TEMPLATE_ID: "template_1v2rubk",
+    YOUR_USER_ID: "user_a8jzumUe9ysYMHjXLrfnR",
+};
+
+const socialprofils = {
+    github: "https://github.com/malikbens/",
+    linkedin: "https://www.linkedin.com/in/malik-benserradj-47b4b3206/",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logoimg,
+};
